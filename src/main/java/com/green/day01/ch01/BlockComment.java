161dea61db6d08c03710ed5c1c52ec6f7c9e6411
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-07 17:27:39 +0900

    first

---
+++ src/main/java/com/green/day01/ch01/BlockComment.java
@@ -0,0 +1,22 @@
+package com.green.day01.ch01;
+/*
+    멀티라인 주석 (여러줄 주석처리 가능)
+ */
+
+
+public class BlockComment {
+
+    public static void main(String[] args) {
+        /* 한줄 처리도 가능 */
+
+        /*
+        작성된 코드인데 주석처리를 하고 싶으면
+        마우스로 블록을 잡고 ctrl + / 처리를 하면 된다. (토글 Toggle 가능)
+         */
+
+        System.out.println(7);
+        System.out.println();
+        System.out.println(3.15);
+
+    }
+}
