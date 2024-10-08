4fd30023f1b1c37a2b7d1b5b02ef5f2f72fcb244
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-08 17:26:24 +0900

    studying

---
+++ src/main/java/com/green/day02/ch05/SwitchStudy.java
@@ -0,0 +1,25 @@
+package com.green.day02.ch05;
+
+public class SwitchStudy {
+
+    public static void main(String[] args) {
+        String name = new String("홍길동");
+        String name2 = new String("홍길동");
+
+        System.out.printf("name: %s\n", name);
+        System.out.printf("name2: %s\n", name2);
+        System.out.printf("name == name2: %b\n", name.equals(name2));
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
