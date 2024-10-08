9c02ab13c05a640ef1fb2f88178e14f6b3d02788
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-08 10:30:01 +0900

    studying

---
+++ src/main/java/com/green/day02/ch04/ArithOp.java
@@ -0,0 +1,15 @@
+package com.green.day02.ch04;
+
+public class ArithOp {
+
+    public static void main(String[] args) {
+        int n1 = 7;
+        int n2 = 3;
+
+        System.out.printf("%d + %d = %d\n", n1, n2, (n1 + n2));
+        System.out.printf("%d - %d = %d\n", n1, n2, (n1 - n2));
+        System.out.printf("%d * %d = %d\n", n1, n2, (n1 * n2));
+        System.out.printf("%d / %d = %d\n", n1, n2, (n1 / n2));
+        System.out.printf("%d %% %d = %d\n", n1, n2, (n1 % n2));
+    }
+}
