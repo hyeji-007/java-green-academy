6f4da765937a91557c712fea571c032fff55bed9
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-09 12:26:24 +0900

    sutdy at home

---
+++ src/main/java/com/green/day02/ch05/MissionIfPr.java
@@ -0,0 +1,30 @@
+package com.green.day02.ch05;
+
+public class MissionIfPr {
+    /*
+    아래 코드에서는 두 개의 if 문을 사용하고 있다.
+    이를 하나의 if문만 사용하도록 변경해보자.
+     */
+    public static void main(String[] args) {
+        int n = 140;
+
+        if (n > 0) {
+            if ( (n % 2) == 0 ) {
+                System.out.printf("%d는 양수이면서 짝수이다.\n", n);
+            }
+        }
+        System.out.println("--끝--");
+
+        if ( n > 0 && ((n % 2) == 0) ) {
+            System.out.printf("%d는 양수이면서 짝수이다.\n", n);
+        }
+
+
+
+
+
+
+
+
+    }
+}
