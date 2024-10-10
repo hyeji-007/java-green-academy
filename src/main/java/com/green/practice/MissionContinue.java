e4dffdf7866f304d9c4dba2f9b121c59929f736b
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-10 21:47:23 +0900

    study at home

---
+++ src/main/java/com/green/practice/MissionContinue.java
@@ -0,0 +1,32 @@
+package com.green.practice;
+
+public class MissionContinue {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        for (int i = 1; i < 100; i++) {
+            if ((i % 5) == 0 && (i % 7) == 0) {
+                count++;
+                System.out.println("i: " + i);
+            }
+        }
+        System.out.println("count: " + count);
+        System.out.println();
+
+
+        int count2 = 0;
+        for (int z = 1; z < 100; z++) {
+            if ((z % 5) != 0 || (z % 7) != 0) {
+                continue;
+            }
+            count2++;
+            System.out.println("z: " + z);
+        }
+        System.out.println("count: " + count);
+
+
+    }
+
+
+}
--- src/main/java/com/green/practice/MissionContinueBreak.java
+++ src/main/java/com/green/practice/MissionContinueBreak.java
@@ -4,22 +4,7 @@ public class MissionContinueBreak {
 
     public static void main(String[] args) {
 
-        int odd = 0;
-        int count = 0;
 
-        for (int i = 1; ; i++) {
-            if ( i % 2 == 1) {
-                count ++;
-                odd += i;
-            }
-
-            if (odd > 1000) {
-                break;
-            }
-        }
-
-        System.out.println("odd = " + odd);
-        System.out.println("count = " + count);
 
     }
 }
