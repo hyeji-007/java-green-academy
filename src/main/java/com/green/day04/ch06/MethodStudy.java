506e05a139ec106945f5e7be49a601a181540c31
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-14 09:41:10 +0900

    study

--- src/main/java/com/green/day04/ch06/MethodStudy.java
+++ src/main/java/com/green/day04/ch06/MethodStudy.java
@@ -11,10 +11,10 @@ public class MethodStudy {
     아래 4가지가 메소드 구성요소
     void: 리턴타입: 메소드 호출된 후 결과값
     main: 메소드명: 호출할 때 사용
-    (String[] args): 파라미터. 매개변수: 메소드에 보내는 데이터
+    (String[] args): 파라미터. 매개변수: 메소드를 호출할 때 메소드에 보내는 데이터
     {}: 구현부: 호출되었을 때 실행되는 부분
     
-    리턴타입 + 메소드명 + 파라미터: 선언부
+    리턴타입 + 메소드명 + 파라미터(매개 변수): 선언부 (<-> 구현부)
 
     메소드를 크게 두 분류
     - void 메소드: 리턴하는 값이 없다. (결과값이 없다.) 실행커서가 단독으로 호출한 곳으로 돌아간다.
@@ -25,7 +25,7 @@ public class MethodStudy {
      */
 
     public static void main(String[] args) { //호출하는 메소드
-        sum(5, 7); //sum메소드 호출 //데이터화가 되지는 않는다.
+        sum(5, 7); //sum메소드 호출 //데이터화가 되지는 않는다. // 메소드에 보내는 값은 아규먼트(인수, 인자)
         sum(15, 17); //sum메소드 호출
         int result = returnSum(5, 7); //int 12
         System.out.println("result: " + result);
@@ -34,7 +34,7 @@ public class MethodStudy {
     }
 
     //void 메소드
-    //sum이라는 이름의 static 메소드를 정의(파라미터 2개)
+    //sum이라는 이름의 static 메소드를 정의(파라미터(매개변수) 2개)
     static void sum(int n1, int n2) {
         System.out.printf("%d + %d = %d\n", n1, n2, (n1 + n2));
     }
