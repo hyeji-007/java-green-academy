c0eb5b1fc6c7aa584f5dce1796ae359f9f947a66
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-15 18:01:41 +0900

    study

--- src/main/java/com/green/day05/ch07/BankAccountPO.java
+++ src/main/java/com/green/day05/ch07/BankAccountPO.java
@@ -1,7 +1,8 @@
 package com.green.day05.ch07;
 
 public class BankAccountPO {
-    static int balance = 0; //static 전역변수 //static이 붙으면 객체화를 안 해도 된다.(static이 안 붙으면 객체화를 해야한다)
+    static int balance = 0; //static 전역변수
+    // static이 붙으면 객체화를 안 해도 된다.(static이 안 붙으면 객체화를 해야한다)
 
     public static int deposit(int amount){ //입금을 담당하는 메소드(행위)
         balance += amount;
