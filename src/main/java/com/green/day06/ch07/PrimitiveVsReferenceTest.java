2dfd36bf81db22251cc73c119016e90492a5196a
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-15 13:15:13 +0900

    study

--- src/main/java/com/green/day06/ch07/PrimitiveVsReferenceTest.java
+++ src/main/java/com/green/day06/ch07/PrimitiveVsReferenceTest.java
@@ -18,16 +18,16 @@ public class PrimitiveVsReferenceTest {
         System.out.println("num: " + num); //10
 
         /*
-        main메소드에 있는 num변수(main-num)dhk changeNum메소드에 있는 num매개변수(m-num)는
+        main메소드에 있는 num변수(main-num)와 changeNum메소드에 있는 num매개변수(m-num)는
         다른 공간이다. main-num이 가지고 있는 값을 복사하여 m-num에 저장했다.
         m-num값을 변경한다고 하여 main-num값이 변경되는 것은 아니다. 왜? 다른 공간이기 때문
          */
 
 
-        NumberBox nb = new NumberBox();
+        NumberBox nb = new NumberBox(); // 1. nb에는 주소값이 저장되어 있다.(0x111)
         nb.num = 10;
-        ChangeNum(nb);
-        System.out.println("nb.num: " + nb.num); //100
+        ChangeNum(nb); //nb: numberBox 객체 주소값
+        System.out.println("nb.num: " + nb.num); // 4. 100
 
 
     }
@@ -36,8 +36,14 @@ public class PrimitiveVsReferenceTest {
         num = 100;
     }
 
-    public static void ChangeNum(NumberBox nb) {
-        nb.num = 100;
+    /*
+    main메소드에 있는 nb변수(main-nb)와 changeNum메소드에 있는 nb 매개변수(m-nb)는 다른 공간이다.
+    다만 같은 값(주소값)을 가지고 있다. 그로 인해 같은 객체에 접근할 수 있다.
+    m-nb를 통해 객체의 num값을 변경을 했다면 main-nb를 통해 접근했을 때 변경된 값을 얻게 된다.
+     */
+
+    public static void ChangeNum(NumberBox nb) { // 2. 다른 공간이지만 같은 주소값(0x111)
+        nb.num = 100; // 3. 10 >> 100 (같은 객체의 주소값이기 때문에 접근이 가능하다)
     }
 
 }
