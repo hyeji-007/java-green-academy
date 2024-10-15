bf0fc4cc0cc18783bf7bc7629919809cc18e7228
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-15 15:50:01 +0900

    study

--- src/main/java/com/green/day06/ch09/AccessModifiers.java
+++ src/main/java/com/green/day06/ch09/AccessModifiers.java
@@ -6,7 +6,9 @@ public class AccessModifiers {
     접근 수준 지시자: private, default, protected, public
 
     private: class내에서만 접근 가능
-    default: private + 같은 패키지면 접근 가능
+    default: private + 같은 패키지면 접근 가능 >> 사용 안함
+    protected: default + 상속 관계면 접근 가능
+    public: 무조건 접근 가능
 
 
      */
