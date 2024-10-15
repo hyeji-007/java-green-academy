0475735c7b3d1429e4efdecf7574285510ab0192
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-15 16:10:25 +0900

    study

--- src/main/java/com/green/day06/ch09/AccessModifiers.java
+++ src/main/java/com/green/day06/ch09/AccessModifiers.java
@@ -3,13 +3,18 @@ package com.green.day06.ch09;
 public class AccessModifiers {
 
     /*
-    접근 수준 지시자: private, default, protected, public
+    접근 수준 지시자: private, default(작성 안함), protected, public
 
     private: class내에서만 접근 가능
     default: private + 같은 패키지면 접근 가능 >> 사용 안함
     protected: default + 상속 관계면 접근 가능
     public: 무조건 접근 가능
 
+    class는 보통 public 붙이고
+    멤버필드는 대부분 private이다.
+        - private를 붙이지 않아도 되는 경우가 있다 >>> 상수는 오픈해도 된다. (어차피 안 변함)
+        - 변수는 무조건 private (은닉화/캡슐화)
+        - private는 멤버필드를 읽기/쓰기할 때 메소드를 통해서한다. (getters/setters)
 
      */
 
---
+++ src/main/java/com/green/day06/ch09/GetterSetterExam.java
@@ -0,0 +1,17 @@
+package com.green.day06.ch09;
+
+public class GetterSetterExam {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+    private float height;
+
+
+
+}
