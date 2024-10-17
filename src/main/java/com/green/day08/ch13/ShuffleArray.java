6a35d17a0341d93bb076e7242fa896532adc1ebf
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-17 17:28:28 +0900

    study

---
+++ src/main/java/com/green/day08/ch13/ShuffleArray.java
@@ -0,0 +1,31 @@
+package com.green.day08.ch13;
+
+import java.util.Arrays;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        //0 ~ 19
+        int temp;
+        for(int i = 0; i < arr.length; i++) {
+            int rIdx = (int)(Math.random() * arr.length);
+
+            temp = arr[i];
+            arr[i] = arr[rIdx];
+            arr[rIdx] = temp;
+
+        }
+
+
+        System.out.println(Arrays.toString(arr));
+
+
+
+
+    }
+}
