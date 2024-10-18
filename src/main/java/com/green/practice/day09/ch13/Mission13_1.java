b834b0b0bc1567e25dce17ccfea1dd9e6a4373cf
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-18 18:10:15 +0900

    study

---
+++ src/main/java/com/green/practice/day09/ch13/Mission13_1.java
@@ -0,0 +1,17 @@
+package com.green.practice.day09.ch13;
+
+public class Mission13_1 {
+    /*
+    int형 1차원 배열을 매개변수로 전달받아서 배열에
+    저장된 최댓값, 최솟값을 찾아서 반환하는 메소드를
+    각각 다음의 형태로 정의하자.
+    public static int minValue(int[] arr) //최솟값 반환
+    public static int maxValue(int[] arr) //최댓값 반환
+
+    단 반복문을 사용할 때 minValue의 정의에서는 일반적인 for문을 사용
+    maxValue의 정의에서는 enhanced-for문을 사용하자.
+     */
+
+
+
+}
