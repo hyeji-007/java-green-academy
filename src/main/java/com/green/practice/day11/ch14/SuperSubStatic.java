20aad85a2c5c63fe8c88cc2bed5ea526e3083731
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-22 18:10:13 +0900

    study

--- src/main/java/com/green/practice/day11/ch14/SuperSubStatic.java
+++ src/main/java/com/green/practice/day11/ch14/SuperSubStatic.java
@@ -1,4 +1,29 @@
 package com.green.practice.day11.ch14;
 
 public class SuperSubStatic {
+
+    public static void main(String[] args) {
+        SuperCLS s1 = new SuperCLS();
+        SuperCLS s2 = new SuperCLS();
+        SubCLS sub1 = new SubCLS();
+        sub1.showCount();
+    }
+}
+
+class SuperCLS {
+    protected static int count; //공용으로 쓰는 static 멤버필드
+
+    SuperCLS() {
+        count++;
+    }
+
+}
+
+class SubCLS extends SuperCLS {
+    SubCLS() {
+        super();
+    }
+    void showCount() {
+        System.out.println("count: " + count);
+    }
 }
