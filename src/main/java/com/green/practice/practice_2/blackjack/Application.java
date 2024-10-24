4072f6d01e9ff2d1a91597d07767772fcc592c6f
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-25 00:59:09 +0900

    study at home

--- src/main/java/com/green/practice/practice_2/blackjack/Application.java
+++ src/main/java/com/green/practice/practice_2/blackjack/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Game game = new Game();
         game.play();
-        Card card = cardDeck.draw();
+        //Card card = cardDeck.draw();
     }
 }
