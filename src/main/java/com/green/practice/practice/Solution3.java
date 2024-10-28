18728953c9c48613f4e8dca38c28453ff87ecdc0
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-28 18:08:38 +0900

    study

---
+++ src/main/java/com/green/practice/practice/Solution3.java
@@ -0,0 +1,27 @@
+package com.green.practice.practice;
+
+public class Solution3 {
+    public int[] solution(int num1, int denom1, int num2, int denom2) {
+        int[] answer = new int[2];
+
+        answer[0] = num1 * denom2 + num2 * denom1;
+        answer[1] = denom1 * denom2;
+
+        int gcd = gcd(answer[0], answer[1]);
+
+        answer[0] /= gcd;
+        answer[1] /= gcd;
+
+        return answer;
+
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
