382dd2cf5fc83c51cf5331070cb1902f7950e0c4
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-29 15:19:48 +0900

    study

--- src/main/java/com/green/day15/ch18/MyExceptionClass.java
+++ src/main/java/com/green/day15/ch18/MyExceptionClass.java
@@ -7,20 +7,20 @@ public class MyExceptionClass {
     public static void main(String[] args) {
         System.out.print("나이 입력: ");
         try {
-            int age = readAge();
+            int age = readAge(); //예외를 던지면
             System.out.printf("입력된 나이: %d\n", age);
-        } catch (ReadAgeException e) {
+        } catch (ReadAgeException e) { //catch가 잡는다
             System.out.println(e.getMessage());
         }
         System.out.println(" -- 끝 -- ");
     }
 
-    public static int readAge() throws ReadAgeException {
+    public static int readAge() throws ReadAgeException { //Trowable을 상속받은 애들만 thorw할 수 있다.
         Scanner scan = new Scanner(System.in);
         int age = scan.nextInt();
         if (age < 0) {
             scan.close();
-            throw new ReadAgeException(); //throw를 만나면 뒷부분 실행 x
+            throw new ReadAgeException(); //throw를 만나면 뒷부분 실행 x // >> e
         }
         System.out.println(">> if문 이후");
         scan.close();
