8468eeb5c01c9b2c1882b28c2503dff7453e9ff0
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-30 15:24:39 +0900

    study

--- src/main/java/com/green/practice/practice/Array1.java
+++ src/main/java/com/green/practice/practice/Array1.java
@@ -12,6 +12,14 @@ public class Array1 {
     }
      */
 
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        int idx = 0;
+        for (int number : numbers) {
+            answer[idx++] = number * 2;
+        }
+        return answer;
+    }
 
 
 
