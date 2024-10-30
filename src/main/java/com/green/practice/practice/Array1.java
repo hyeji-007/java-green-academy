a92ab7cf8ae33f585cfe8359d5eca2db3f6c7357
KwonHyeji <kwonhyeji007@gmail.com>
2024-10-30 17:36:41 +0900

    study

--- src/main/java/com/green/practice/practice/Array1.java
+++ src/main/java/com/green/practice/practice/Array1.java
@@ -11,6 +11,54 @@ public class Array1 {
         return answer;
     }
      */
+    public class ManualSortExample {
+        public static void main(String[] args) {
+            int[] array = {5, 2, 8, 1, 3};
+
+            // 오름차순 정렬
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 0; j < array.length - 1 - i; j++) {
+                    if (array[j] > array[j + 1]) {
+                        // 두 요소를 교환
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            System.out.print("오름차순 정렬: ");
+            for (int num : array) {
+                System.out.print(num + " ");
+            }
+        }
+
+    }
+    public class ManualSortExample2 {
+        public static void main(String[] args) {
+            int[] array = {5, 2, 8, 1, 3};
+
+            // 내림차순 정렬
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 0; j < array.length - 1 - i; j++) {
+                    if (array[j] < array[j + 1]) {
+                        // 두 요소를 교환
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            System.out.print("내림차순 정렬: ");
+            for (int num : array) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+
+
 
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
@@ -20,6 +68,9 @@ public class Array1 {
         }
         return answer;
     }
+
+
+
 
 
 
