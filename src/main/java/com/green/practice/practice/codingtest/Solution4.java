3f44085e33552513b270a10444a17b02577eb0e3
KwonHyeji <kwonhyeji007@gmail.com>
2024-11-01 19:07:52 +0900

    study

---
+++ src/main/java/com/green/practice/practice/codingtest/Solution4.java
@@ -0,0 +1,21 @@
+package com.green.practice.practice.codingtest;
+
+import java.util.ArrayList;
+
+class Solution4 {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i += 2) {
+            list.add(i);
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
+
